d0466d3732fc70f2783ae870b140f340d00fb25a
poloniumoxide <ericyang256@gmail.com>
2025-07-27 13:07:33 -0600

    Update main.cpp

--- droydfent/Template/src/main.cpp
+++ droydfent/Template/src/main.cpp
@@ -1,9 +1,15 @@
 #include <raylib.h>
 #include <raymath.h>
 
+#include <vector>
+
+using namespace std;
+
 int main()
 {
 	InitWindow(400, 224, "george droyd's fentanyl");
+
+	vector<int> vectortest = {0, 1, 2};
 	
 	while (!WindowShouldClose()) {
         	BeginDrawing();
