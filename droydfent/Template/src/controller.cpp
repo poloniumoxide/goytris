9c2f403cfe75e3072b7382c59b94020b53871a0f
poloniumoxide <ericyang256@gmail.com>
2025-07-27 14:47:30 -0600

    goysloppe removed

---
+++ droydfent/Template/src/controller.cpp
@@ -0,0 +1,81 @@
+#include "defaultdata.cpp"
+
+#include <deque>
+
+using namespace std;
+
+class Controller {
+public:
+
+    deque<int> inputs;
+    vector<int> held;
+    vector<int> order = {kleft, kright, kcw, kccw, k180, khold, ksd, khd}
+
+    Controller() {
+        held = {0, 0, 0, 0, 0, 0, 0, 0} //8, l, r, cw, ccw, 180, hold, sd, hd 
+        inputs = {};
+    }
+
+    void run() {
+        
+        for (int i = 0; i < order.size(); i ++) {
+            checkheld(order[i], i);
+        }
+        if (held[kleft] >= das) {
+            int p = held[kleft]-das;
+            if (p%arr == 0) {
+                inputs.push_back(2)
+            }
+        }
+        if (held[kright] >= das) {
+            int p = held[kright]-das;
+            if (p%arr == 0) {
+                inputs.push_back(3)
+            }
+        }
+        if (held[kcw] == 1) {
+            inputs.push_back(6)
+        }
+        if (held[kccw] == 1) {
+            inputs.push_back(8)
+        }
+        if (held[k180] == 1) {
+            inputs.push_back(7)
+        }
+        if (held[khold] == 1) {
+            inputs.push_back(9)
+        }
+        if (held[ksd] > 0) {
+            if (sdf == 0) {
+                for (int i = 0; i < 40; i++) {
+                    inputs.push_back(1)
+                }
+            } else if (held[ksd]%sdf == 0) {
+                inputs.push_back(1)
+            }
+        }
+        if (held[khd] == 1) {
+            inputs.push_back(0)
+        }
+
+    }
+
+    void checkheld(int key, int pos) {
+        if (IsKeyDown(key)) {
+            held[pos]++;
+        } else {
+            held[pos] = 0;
+        }
+    }
+
+    int next() {
+        if (!inputs.empty()) {
+            int t = inputs.front();
+            inputs.pop_front();
+            return t;
+        } else {
+            return 271000;
+        }
+    }
+
+};
