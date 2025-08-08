16183ae07455bd70778a015c0c91d6007a599d74
poloniumoxide <ericyang256@gmail.com>
2025-08-08 14:41:34 -0600

    added forces (not checked yet)

--- droydfent/Template/src/force.h
+++ droydfent/Template/src/force.h
@@ -2,24 +2,33 @@
 #define FORCE_H
 
 #include <vector>
+#include <deque>
 
 using namespace std;
 
 class Force {
 public:
 
-    vector<vector<int>> f;
+    struct Component {
+        double strength;
+        double entropy;
+        vector<bool> holes; // default: 10 long
+    };
+
+    deque<Component> fnet;
 
     int length;
     int strength;
 
-    Force(int, int);
+    Force(double = 0, double = 0, vector<bool> = {false, false, false, false, false, false, false, false, false, false});
 
     void merge(Force);
-
     void cancel(Force);
+    void clear();
 
+    static vector<bool> genholes(int, int = 10);
+    static void compcancel(Component&, Component&);
 
 };
 
-#endif // MY_HEADER_H
+#endif
